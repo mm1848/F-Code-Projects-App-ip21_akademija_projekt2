8d39bc783abfbf8a71beb4d61609e2ef81a64299
Barbara <zverbarbara@gmai.com>
2024-03-27 18:27:14 +0100

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    // Relationships
+    public function favourites()
+    {
+        return $this->hasMany(Favourite::class, 'user_id');
+    }
+}
